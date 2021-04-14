d98bd7a27c37f758ec43d0c56590973b436ad6c2
reven <cutefishos@foxmail.com>
2021-04-15 00:52:02 +0800

    Session: disable SESSION_MANAGER

--- session/main.cpp
+++ session/main.cpp
@@ -1,8 +1,18 @@
 #include "application.h"
+#include <QQuickWindow>
 
 int main(int argc, char *argv[])
 {
+    putenv((char *)"SESSION_MANAGER=");
+
+    // force xcb QPA plugin as session manager server is very X11 specific.
+    qputenv("QT_QPA_PLATFORM", QByteArrayLiteral("xcb"));
+
+    QQuickWindow::setDefaultAlphaBuffer(true);
+    QCoreApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
+
     Application a(argc, argv);
     a.setQuitOnLastWindowClosed(false);
+
     return a.exec();
 }
