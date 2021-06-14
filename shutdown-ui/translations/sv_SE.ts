80b86c0aeb9a52204d6210f9ef8ba9c1626f192a
Luna Jernberg <droidbittin@gmail.com>
2021-06-14 16:13:00 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CutefishOS/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/shutdown-ui/sv/

--- shutdown-ui/translations/sv_SE.ts
+++ shutdown-ui/translations/sv_SE.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="67"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>Stäng av</translation>
     </message>
     <message>
         <location filename="../main.qml" line="75"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Starta om</translation>
     </message>
     <message>
         <location filename="../main.qml" line="83"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Logga ut</translation>
     </message>
     <message>
         <location filename="../main.qml" line="91"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Viloläge</translation>
     </message>
 </context>
 </TS>
