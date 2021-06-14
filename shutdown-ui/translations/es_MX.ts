80a7f2fc70696a5c11b8d1e58eea3701d1d91b70
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2021-06-14 22:26:43 +0000

    Translated using Weblate (Spanish (Mexico))
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CutefishOS/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/shutdown-ui/es_MX/

--- shutdown-ui/translations/es_MX.ts
+++ shutdown-ui/translations/es_MX.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="67"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>Apagar</translation>
     </message>
     <message>
         <location filename="../main.qml" line="75"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Reiniciar</translation>
     </message>
     <message>
         <location filename="../main.qml" line="83"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Cerrar Sesión</translation>
     </message>
     <message>
         <location filename="../main.qml" line="91"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Suspender</translation>
     </message>
 </context>
 </TS>
