1139af9776158edf0442d3606152a09d235d2bd6
Weblate <noreply@weblate.org>
2021-06-15 15:03:17 +0200

    Added translation using Weblate (Esperanto)

---
+++ settings-daemon/translations/eo_XX.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="eo" sourcelanguage="de_DE">
+<context>
+    <name>Battery</name>
+    <message>
+        <source>d</source>
+        <translation type="vanished">Tage</translation>
+    </message>
+    <message>
+        <source>h</source>
+        <translation type="vanished">Stunden</translation>
+    </message>
+    <message>
+        <source>m</source>
+        <translation type="vanished">Minuten</translation>
+    </message>
+    <message>
+        <source>until fully charged.</source>
+        <translation type="vanished">bis Akku voll.</translation>
+    </message>
+    <message>
+        <source>remaining.</source>
+        <translation type="vanished">übrig.</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="268"/>
+        <source>%1d</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="274"/>
+        <source>%1h</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="281"/>
+        <source>%1m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="290"/>
+        <source>%1 until fully charged</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="292"/>
+        <source>%1 remaining</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="294"/>
+        <source>Fully charged.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="317"/>
+        <source>now</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="319"/>
+        <source>%1 ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ago</source>
+        <translation type="vanished">vergangen</translation>
+    </message>
+</context>
+<context>
+    <name>UPowerDevice</name>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="71"/>
+        <source>%1 Battery</source>
+        <comment>%1 is battery technology</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="87"/>
+        <source>Lithium-ion</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="89"/>
+        <source>Lithium Polymer</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="91"/>
+        <source>Lithium Iron Phosphate</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="93"/>
+        <source>Lead Acid</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="95"/>
+        <source>Nickel Cadmium</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="97"/>
+        <source>Nickel Metal Hydride</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="99"/>
+        <source>Unknown</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
