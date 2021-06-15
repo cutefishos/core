208685d2631f8c73b8fa2ccf9813081a1ca490c9
Jan Kožíšek <hlubinix@gmail.com>
2021-06-15 10:59:19 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: CutefishOS/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/polkit-agent/cs/

--- polkit-agent/translations/cs_CZ.ts
+++ polkit-agent/translations/cs_CZ.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="67"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Heslo</translation>
     </message>
     <message>
         <location filename="../main.qml" line="94"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Hotovo</translation>
     </message>
     <message>
         <location filename="../main.qml" line="86"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Zrušit</translation>
     </message>
 </context>
 </TS>
