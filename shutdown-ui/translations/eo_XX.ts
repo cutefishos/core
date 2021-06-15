4573a9c0d4825b43bf07854c7a6f8984a1df347a
Weblate <noreply@weblate.org>
2021-06-15 15:03:22 +0200

    Added translation using Weblate (Esperanto)

---
+++ shutdown-ui/translations/eo_XX.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="eo">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="67"/>
+        <source>Shutdown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="75"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="83"/>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="91"/>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
