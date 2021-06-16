4c4595fc2113451eef8fbc30fbbb160c6e668910
J. Lavoie <j.lavoie@net-c.ca>
2021-06-16 11:30:52 +0000

    Translated using Weblate (Esperanto)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: CutefishOS/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/polkit-agent/eo/

--- polkit-agent/translations/eo_XX.ts
+++ polkit-agent/translations/eo_XX.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="67"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Pasvorto</translation>
     </message>
     <message>
         <location filename="../main.qml" line="94"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Finita</translation>
     </message>
     <message>
         <location filename="../main.qml" line="86"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuligi</translation>
     </message>
 </context>
 </TS>
