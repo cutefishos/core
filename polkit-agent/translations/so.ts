89fd6925469ad0e9dfc539d983e261b549d6f1da
Hosted Weblate <hosted@weblate.org>
2021-06-22 18:33:48 +0200

    Translated using Weblate (Somali)
    Currently translated at 100.0% (3 of 3 strings)
    
    Co-authored-by: Beendiid <beendiid@mail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/polkit-agent/so/
    Translation: CutefishOS/Polkit Agent

--- polkit-agent/translations/so.ts
+++ polkit-agent/translations/so.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="67"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Erey sireed</translation>
     </message>
     <message>
         <location filename="../main.qml" line="94"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Dhammee</translation>
     </message>
     <message>
         <location filename="../main.qml" line="86"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Xir</translation>
     </message>
 </context>
 </TS>
