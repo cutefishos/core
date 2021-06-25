724ed37a54db8a698196e9b7c224d48f19f6ec8f
Hosted Weblate <hosted@weblate.org>
2021-06-25 13:09:23 +0200

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Added translation using Weblate (Hungarian)
    
    Co-authored-by: Benjamin Kőhidi <benstone326@gmail.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/shutdown-ui/hu/
    Translation: CutefishOS/Shutdown UI

---
+++ shutdown-ui/translations/hu_HU.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="67"/>
+        <source>Shutdown</source>
+        <translation>Leállítás</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="75"/>
+        <source>Reboot</source>
+        <translation>Újraindítás</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="83"/>
+        <source>Logout</source>
+        <translation>Kijelentkezés</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="91"/>
+        <source>Suspend</source>
+        <translation>Felfüggesztés</translation>
+    </message>
+</context>
+</TS>
