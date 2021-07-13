bf9932e6799a0d8816bcaf6224a3dc0644685098
Hosted Weblate <hosted@weblate.org>
2021-07-13 21:32:56 +0200

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (4 of 4 strings)
    
    Added translation using Weblate (Tamil)
    
    Translated using Weblate (Bosnian)
    
    Currently translated at 100.0% (4 of 4 strings)
    
    Co-authored-by: Emin Macanović <macanovic.emin@gmail.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: MC Naveen <funmemcn@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/shutdown-ui/bs/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/shutdown-ui/ta/
    Translation: CutefishOS/Shutdown UI

--- shutdown-ui/translations/ta_IN.ts
+++ shutdown-ui/translations/ta_IN.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="bs">
+<TS version="2.1" language="ta">
 <context>
     <name>main</name>
     <message>
         <location filename="../main.qml" line="67"/>
         <source>Shutdown</source>
-        <translation>Ugasiti</translation>
+        <translation>நிறுத்தவும்</translation>
     </message>
     <message>
         <location filename="../main.qml" line="75"/>
         <source>Reboot</source>
-        <translation>Ponovno pokreni</translation>
+        <translation>மீண்டும் துவக்கவும்</translation>
     </message>
     <message>
         <location filename="../main.qml" line="83"/>
         <source>Logout</source>
-        <translation>Odjavi se</translation>
+        <translation>வெளியேறு</translation>
     </message>
     <message>
         <location filename="../main.qml" line="91"/>
         <source>Suspend</source>
-        <translation>Suspendiraj</translation>
+        <translation>இடைநிறுத்து</translation>
     </message>
 </context>
 </TS>
