128c644a51f81b8b9468b7bd24dfd726702f954d
Hosted Weblate <hosted@weblate.org>
2021-07-22 09:07:18 +0200

    Translated using Weblate (Sinhala)
    Currently translated at 25.0% (1 of 4 strings)
    
    Added translation using Weblate (Sinhala)
    
    Co-authored-by: HelaBasa <R45XvezA@protonmail.ch>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/shutdown-ui/si/
    Translation: CutefishOS/Shutdown UI

---
+++ shutdown-ui/translations/si_LK.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="si">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="67"/>
+        <source>Shutdown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="75"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="83"/>
+        <source>Logout</source>
+        <translation>නික්මෙන්න</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="91"/>
+        <source>Suspend</source>
+        <translation>අත්හිටුවන්න</translation>
+    </message>
+</context>
+</TS>
