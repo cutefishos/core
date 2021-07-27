0616f899578acaa229137ab3066fb071cfe3e84b
reionwong <reionwong@gmail.com>
2021-07-27 22:56:33 +0800

    ShutdownUI: update translations

--- shutdown-ui/translations/en_US.ts
+++ shutdown-ui/translations/en_US.ts
@@ -4,22 +4,27 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.qml" line="67"/>
+        <location filename="../main.qml" line="74"/>
         <source>Shutdown</source>
         <translation>Shutdown</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="75"/>
+        <location filename="../main.qml" line="82"/>
         <source>Reboot</source>
         <translation>Reboot</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="83"/>
+        <location filename="../main.qml" line="90"/>
         <source>Logout</source>
         <translation>Log out</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="91"/>
+        <location filename="../main.qml" line="98"/>
+        <source>Lock screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="106"/>
         <source>Suspend</source>
         <translation>Suspend</translation>
     </message>
--- shutdown-ui/translations/zh_CN.ts
+++ shutdown-ui/translations/zh_CN.ts
@@ -4,22 +4,27 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.qml" line="67"/>
+        <location filename="../main.qml" line="74"/>
         <source>Shutdown</source>
         <translation>关机</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="75"/>
+        <location filename="../main.qml" line="82"/>
         <source>Reboot</source>
         <translation>重新启动</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="83"/>
+        <location filename="../main.qml" line="90"/>
         <source>Logout</source>
         <translation>注销</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="91"/>
+        <location filename="../main.qml" line="98"/>
+        <source>Lock screen</source>
+        <translation>锁屏</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="106"/>
         <source>Suspend</source>
         <translation>休眠</translation>
     </message>
