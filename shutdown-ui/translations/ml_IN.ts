065c4006fbc69cd29e98e2b2f2bcc6ef0a859f42
Hosted Weblate <hosted@weblate.org>
2021-08-14 17:12:19 +0200

    Added translation using Weblate (Malayalam)
    Co-authored-by: Weblate <noreply@weblate.org>

---
+++ shutdown-ui/translations/ml_IN.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ml">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="74"/>
+        <source>Shutdown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="82"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="90"/>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="98"/>
+        <source>Lock screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="106"/>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
