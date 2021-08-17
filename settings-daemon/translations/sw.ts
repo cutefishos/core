e0e4c8cddfdcb26f738b153b01f1e3df7d42cfee
Hosted Weblate <hosted@weblate.org>
2021-08-17 23:53:12 +0200

    Translated using Weblate (Swahili)
    Currently translated at 100.0% (16 of 16 strings)
    
    Added translation using Weblate (Swahili)
    
    Added translation using Weblate (Romanian)
    
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: circonfl3x <sayalelowesi456@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/settings-daemon/sw/
    Translation: CutefishOS/Settings Daemon

---
+++ settings-daemon/translations/sw.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sw" sourcelanguage="de_DE">
+<context>
+    <name>Battery</name>
+    <message>
+        <source>d</source>
+        <translation type="vanished">Tage</translation>
+    </message>
+    <message>
+        <source>h</source>
+        <translation type="vanished">Stunden</translation>
+    </message>
+    <message>
+        <source>m</source>
+        <translation type="vanished">Minuten</translation>
+    </message>
+    <message>
+        <source>until fully charged.</source>
+        <translation type="vanished">bis Akku voll.</translation>
+    </message>
+    <message>
+        <source>remaining.</source>
+        <translation type="vanished">übrig.</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="268"/>
+        <source>%1d</source>
+        <translation>%1d</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="274"/>
+        <source>%1h</source>
+        <translation>%1h</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="281"/>
+        <source>%1m</source>
+        <translation>%1m</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="290"/>
+        <source>%1 until fully charged</source>
+        <translation>%1 mbaaka imejaa</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="292"/>
+        <source>%1 remaining</source>
+        <translation>%1 imebaki</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="294"/>
+        <source>Fully charged.</source>
+        <translation>Imejaa.</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="317"/>
+        <source>now</source>
+        <translation>Sasa</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="319"/>
+        <source>%1 ago</source>
+        <translation>%1 imepita</translation>
+    </message>
+    <message>
+        <source>ago</source>
+        <translation type="vanished">vergangen</translation>
+    </message>
+</context>
+<context>
+    <name>UPowerDevice</name>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="71"/>
+        <source>%1 Battery</source>
+        <comment>%1 is battery technology</comment>
+        <translation>%1 Betri</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="87"/>
+        <source>Lithium-ion</source>
+        <comment>battery technology</comment>
+        <translation>Lithium-ion</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="89"/>
+        <source>Lithium Polymer</source>
+        <comment>battery technology</comment>
+        <translation>Lithium Polymer</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="91"/>
+        <source>Lithium Iron Phosphate</source>
+        <comment>battery technology</comment>
+        <translation>Lithium Iron Phosphate</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="93"/>
+        <source>Lead Acid</source>
+        <comment>battery technology</comment>
+        <translation>Lead Acid</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="95"/>
+        <source>Nickel Cadmium</source>
+        <comment>battery technology</comment>
+        <translation>Nickel Cadmium</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="97"/>
+        <source>Nickel Metal Hydride</source>
+        <comment>battery technology</comment>
+        <translation>Nickel Metal Hydride</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="99"/>
+        <source>Unknown</source>
+        <comment>battery technology</comment>
+        <translation>Haijulikani</translation>
+    </message>
+</context>
+</TS>
