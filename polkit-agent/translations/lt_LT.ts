209031cb5b241262c983fcefef411bbc6491fe8b
Hosted Weblate <hosted@weblate.org>
2021-09-07 14:00:43 +0200

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translated using Weblate (Lithuanian)
    
    Currently translated at 100.0% (3 of 3 strings)
    
    Added translation using Weblate (Lithuanian)
    
    Co-authored-by: Ahmad Dakhlallah <ahmad@linuxarabia.co>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Moo <hazap@hotmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/polkit-agent/ar/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/polkit-agent/lt/
    Translation: CutefishOS/Polkit Agent

--- polkit-agent/translations/lt_LT.ts
+++ polkit-agent/translations/lt_LT.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ar" sourcelanguage="de_DE">
+<TS version="2.1" language="lt" sourcelanguage="de_DE">
 <context>
     <name>main</name>
     <message>
         <location filename="../main.qml" line="67"/>
         <source>Password</source>
-        <translation>كلمة المرور</translation>
+        <translation>Slaptažodis</translation>
     </message>
     <message>
         <location filename="../main.qml" line="94"/>
         <source>Done</source>
-        <translation>تم</translation>
+        <translation>Atlikta</translation>
     </message>
     <message>
         <location filename="../main.qml" line="86"/>
         <source>Cancel</source>
-        <translation>إلغاﺀ</translation>
+        <translation>Atsisakyti</translation>
     </message>
 </context>
 </TS>
