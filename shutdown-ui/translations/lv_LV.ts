c4df75d291cf46b1d76abbac143078125c90baf9
Hosted Weblate <hosted@weblate.org>
2021-09-12 15:33:46 +0200

    Translated using Weblate (Latvian)
    Currently translated at 100.0% (5 of 5 strings)
    
    Added translation using Weblate (Latvian)
    
    Translated using Weblate (Hebrew (Israel))
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Added translation using Weblate (Hebrew (Israel))
    
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: bugthug <avivyodfat@protonmail.com>
    Co-authored-by: ツ GUCKII ツ <mariusskrasavcevs@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/shutdown-ui/he_IL/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/shutdown-ui/lv/
    Translation: CutefishOS/Shutdown UI

---
+++ shutdown-ui/translations/lv_LV.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lv">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="74"/>
+        <source>Shutdown</source>
+        <translation>Izslēgt</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="82"/>
+        <source>Reboot</source>
+        <translation>Restartēt</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="90"/>
+        <source>Logout</source>
+        <translation>Izrakstīties</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="98"/>
+        <source>Lock screen</source>
+        <translation>Bloķēt ekrānu</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="106"/>
+        <source>Suspend</source>
+        <translation>Aizmidzināt</translation>
+    </message>
+</context>
+</TS>
