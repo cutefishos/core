58601e62abde99d5a148924cd6c24c4239d44547
Hosted Weblate <hosted@weblate.org>
2021-09-12 15:33:46 +0200

    Translated using Weblate (Latvian)
    Currently translated at 100.0% (16 of 16 strings)
    
    Added translation using Weblate (Latvian)
    
    Translated using Weblate (Hebrew (Israel))
    
    Currently translated at 56.2% (9 of 16 strings)
    
    Added translation using Weblate (Hebrew)
    
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: bugthug <avivyodfat@protonmail.com>
    Co-authored-by: ツ GUCKII ツ <mariusskrasavcevs@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/settings-daemon/he_IL/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/settings-daemon/lv/
    Translation: CutefishOS/Settings Daemon

---
+++ settings-daemon/translations/he_IL.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="he" sourcelanguage="de_DE">
+<context>
+    <name>Battery</name>
+    <message>
+        <source>d</source>
+        <translation type="vanished">Tage</translation>
+    </message>
+    <message>
+        <source>h</source>
+        <translation type="vanished">Stunden</translation>
+    </message>
+    <message>
+        <source>m</source>
+        <translation type="vanished">Minuten</translation>
+    </message>
+    <message>
+        <source>until fully charged.</source>
+        <translation type="vanished">bis Akku voll.</translation>
+    </message>
+    <message>
+        <source>remaining.</source>
+        <translation type="vanished">übrig.</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="268"/>
+        <source>%1d</source>
+        <translation>%1d</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="274"/>
+        <source>%1h</source>
+        <translation type="unfinished">%1h</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="281"/>
+        <source>%1m</source>
+        <translation type="unfinished">%1m</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="290"/>
+        <source>%1 until fully charged</source>
+        <translation>%1 עד לסוללה מלאה</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="292"/>
+        <source>%1 remaining</source>
+        <translation>נשאר %1</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="294"/>
+        <source>Fully charged.</source>
+        <translation>סוללה מלאה.</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="317"/>
+        <source>now</source>
+        <translation>עכשיו</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="319"/>
+        <source>%1 ago</source>
+        <translation>לפני %1</translation>
+    </message>
+    <message>
+        <source>ago</source>
+        <translation type="vanished">vergangen</translation>
+    </message>
+</context>
+<context>
+    <name>UPowerDevice</name>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="71"/>
+        <source>%1 Battery</source>
+        <comment>%1 is battery technology</comment>
+        <translation type="unfinished">%1 סוללה</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="87"/>
+        <source>Lithium-ion</source>
+        <comment>battery technology</comment>
+        <translation>ליתיום יון</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="89"/>
+        <source>Lithium Polymer</source>
+        <comment>battery technology</comment>
+        <translation>ליתיום פולימר</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="91"/>
+        <source>Lithium Iron Phosphate</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished">Lithium Iron Phosphate</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="93"/>
+        <source>Lead Acid</source>
+        <comment>battery technology</comment>
+        <translation>חומצת לֵד</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="95"/>
+        <source>Nickel Cadmium</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished">Nickel Cadmium</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="97"/>
+        <source>Nickel Metal Hydride</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished">Nickel Metal Hydride</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="99"/>
+        <source>Unknown</source>
+        <comment>battery technology</comment>
+        <translation type="unfinished">לא ידוע</translation>
+    </message>
+</context>
+</TS>
---
+++ settings-daemon/translations/lv_LV.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lv" sourcelanguage="de_DE">
+<context>
+    <name>Battery</name>
+    <message>
+        <source>d</source>
+        <translation type="vanished">Tage</translation>
+    </message>
+    <message>
+        <source>h</source>
+        <translation type="vanished">Stunden</translation>
+    </message>
+    <message>
+        <source>m</source>
+        <translation type="vanished">Minuten</translation>
+    </message>
+    <message>
+        <source>until fully charged.</source>
+        <translation type="vanished">bis Akku voll.</translation>
+    </message>
+    <message>
+        <source>remaining.</source>
+        <translation type="vanished">übrig.</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="268"/>
+        <source>%1d</source>
+        <translation>%1d</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="274"/>
+        <source>%1h</source>
+        <translation>%1st</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="281"/>
+        <source>%1m</source>
+        <translation>%1m</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="290"/>
+        <source>%1 until fully charged</source>
+        <translation>%1 līdz pilnīgai uzlādei</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="292"/>
+        <source>%1 remaining</source>
+        <translation>%1 atliek</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="294"/>
+        <source>Fully charged.</source>
+        <translation>Pilnīgi uzlādēts.</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="317"/>
+        <source>now</source>
+        <translation>tagad</translation>
+    </message>
+    <message>
+        <location filename="../battery/battery.cpp" line="319"/>
+        <source>%1 ago</source>
+        <translation>pirms %1</translation>
+    </message>
+    <message>
+        <source>ago</source>
+        <translation type="vanished">vergangen</translation>
+    </message>
+</context>
+<context>
+    <name>UPowerDevice</name>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="71"/>
+        <source>%1 Battery</source>
+        <comment>%1 is battery technology</comment>
+        <translation>%1 Akumulators</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="87"/>
+        <source>Lithium-ion</source>
+        <comment>battery technology</comment>
+        <translation>Litija-Jona</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="89"/>
+        <source>Lithium Polymer</source>
+        <comment>battery technology</comment>
+        <translation>Litija Polimēra</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="91"/>
+        <source>Lithium Iron Phosphate</source>
+        <comment>battery technology</comment>
+        <translation>Litija dzelzs fosfāts</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="93"/>
+        <source>Lead Acid</source>
+        <comment>battery technology</comment>
+        <translation>Svina Skābe</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="95"/>
+        <source>Nickel Cadmium</source>
+        <comment>battery technology</comment>
+        <translation>Niķeļa Kadmijs</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="97"/>
+        <source>Nickel Metal Hydride</source>
+        <comment>battery technology</comment>
+        <translation>Niķeļa Metāla Hidrīds</translation>
+    </message>
+    <message>
+        <location filename="../battery/upowerdevice.cpp" line="99"/>
+        <source>Unknown</source>
+        <comment>battery technology</comment>
+        <translation>Nezināms</translation>
+    </message>
+</context>
+</TS>
