98b3053c1e29b4c13612f8a6e859766bcc6ce47c
Reion Wong <reionwong@gmail.com>
2026-09-02 12:24:50 -0400

    refactor(core): drop no-op Qt5 high DPI attributes

--- polkit-agent/main.cpp
+++ polkit-agent/main.cpp
@@ -27,7 +27,6 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     app.setQuitOnLastWindowClosed(false);
 
--- shutdown-ui/main.cpp
+++ shutdown-ui/main.cpp
@@ -30,8 +30,6 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
 
     if (!QDBusConnection::sessionBus().registerService("com.cutefish.ShutdownUI")) {
